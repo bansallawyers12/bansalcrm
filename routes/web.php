81d645f9e49b5fcd1122510140c7815fe1bc4ee3
bansallawyers12@gmail.com <bansallawyers12@gmail.com>
2026-01-05 19:59:25 +1100

    Update routing for Admin actions and remove agent routes
    - Removed the agent routes from the web.php file to streamline the routing structure.
    - Added a new route for completing actions in the Admin panel, enhancing the functionality of the AdminController.
    - Total: 1 file modified, improving the organization and capabilities of the routing system.

--- routes/web.php
+++ routes/web.php
@@ -108,8 +108,6 @@ Route::get('thankyou', 'HomeController@thankyou')->name('thankyou');
 Route::get('/', 'Auth\AdminLoginController@showLoginForm')->name('login');
 Route::post('/', 'Auth\AdminLoginController@login');
 
-/*---------------Agent Route-------------------*/
-require __DIR__ . '/agent.php';
 /*********************Admin Panel Start ***********************/
 Route::prefix('admin')->group(function() {
 	
@@ -136,6 +134,7 @@ Route::prefix('admin')->group(function() {
 		Route::post('/delete_action', 'Admin\AdminController@deleteAction');
          Route::post('/delete_slot_action', 'Admin\AdminController@deleteSlotAction');
 		Route::post('/move_action', 'Admin\AdminController@moveAction');
+		Route::post('/complete-action', 'Admin\AdminController@completeAction')->name('admin.complete-action');
 		
 		
 		Route::post('/add_ckeditior_image', 'Admin\AdminController@addCkeditiorImage')->name('add_ckeditior_image');
